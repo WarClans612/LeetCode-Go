5b2872ad9652204f6e2847e392d81c113f5322a7
Wilbert <wilbert.phen@gmail.com>
2022-01-22 10:24:58 +0800

    Finish implementing stone-game-iv problem in Go

---
+++ 1510.stone-game-iv/02_dp.go
@@ -0,0 +1,15 @@
+package problem1510
+
+func winnerSquareGame(n int) bool {
+	dp := make([]bool, n+1)
+
+	for i := 0; i < n+1; i++ {
+		for k := 1; k*k <= i; k++ {
+			if !dp[i-k*k] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[n]
+}
